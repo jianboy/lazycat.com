84819c0eb299d1a55ac5aa3af19b0650c76fe129
jianboy <liuyuqi.gov@msn.cn>
2023-05-15 02:36:33 +0000

    0

---
+++ src/pages/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home">
+        <el-container height="100%">
+            <el-aside width="100px">
+                <Nav></Nav>
+            </el-aside>
+        </el-container>
+    </div>
+</template>
+
+<script>
+import { Nav } from "../components/Nav.vue";
+</script>
+
+<style lang="scss" scoped>
+.home {
+    width: 90vw;
+    height: 90vh;
+    background-color: rgb(39, 42, 55);
+    border-radius: 15px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+}
+</style>
